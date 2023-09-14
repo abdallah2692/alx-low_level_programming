1e9e1b914d39f0aa60e250e9fdb1bcbe2d7f9a83
abdallah2692 <abdallahmotawea0pl@gmail.com>
2023-09-14 22:57:05 +0300

    me

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,11 +4,12 @@
  *
  * print_alphabet_x10 - funstion that prints the alphabet 10 times
  *
-*/
+ */
 
 void print_alphabet_x10(void)
 {
     int line, ch;
+
     for (line = 0; line <= 9; line++)
     {
         for (ch = 'a'; ch <= 'z'; ch++)
