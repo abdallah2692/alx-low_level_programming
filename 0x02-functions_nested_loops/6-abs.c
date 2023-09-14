0ca1a07f0c04bb42f7781d197ec49edc569e0b59
abdallah2692 <abdallahmotawea0pl@gmail.com>
2023-09-15 00:01:12 +0300

    ff

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,12 +4,12 @@
  * _abs - computes the absolute value
  * Description: the abso;ute digit
  * @n: tack the input integer
- * REturn: always 0
+ * Return: always n
  */
 
 int _abs(int n)
 {
-	if ( n < 0 )
+	if (n < 0)
 		n = ((-1) * n);
 	return (n);
 
