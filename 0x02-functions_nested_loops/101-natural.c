1e12b4c64ffe7746aa31c565b190e0c26bef99b5
abdallah2692 <abdallahmotawea0pl@gmail.com>
2023-09-15 01:39:47 +0300

    ff

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: computethe sum
+ * Return: 0
+ */
+
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num < 10; num++)
+	{
+		if ((num % 3 == 0) || (num %5 == 0)
+				sum += num;
+	}
+	printf("%d\n", sum);
+	
+	return(0);
+}
