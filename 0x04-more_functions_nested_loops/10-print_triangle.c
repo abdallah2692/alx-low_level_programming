fd61abd21f410a3dc8c0344c3324d50fa1989cab
abdallah2692 <abdallahmotawea0pl@gmail.com>
2023-09-16 04:38:41 +0300

    ff

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,47 +7,26 @@
  */
 void print_triangle(int size)
 {
-        if (size <= 0)
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
 
-        {
-
-                _putchar('\n');
-
-        }
-
-        else
-
-        {
-
-                int i, j;
-
-
-                for (i = 1; i <= size; i++)
+		for (i = 1; i <= size; i++)
 
                 {
-
-                        for (j = i; j < size; j++)
-
-                        {
-
-                                _putchar(' ');
-
-                        }
-
-
-                        for (j = 1; j <= i; j++)
-
-                        {
-
-                                _putchar('#');
-
-                        }
-
-
-                        _putchar('\n');
-
-                }
-
-        }
-
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
