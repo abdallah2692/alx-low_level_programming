1b5a36c43bf94e430440a9ce26429e8709a652bd
abdallah2692 <abdallahmotawea0pl@gmail.com>
2023-09-26 13:19:40 +0300

    pointers

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,13 +7,13 @@
 int main(void)
 {
 int n;
-int a[5];
+int b[5];
 int *p;
-a[2] = 1024;
+b[2] = 1024;
 p = &n;
 
 *(p + 5) = 98;
 
-printf("a[2] = %d\n", a[2]);
+printf("b[2] = %d\n", b[2]);
 return (0);
 }
