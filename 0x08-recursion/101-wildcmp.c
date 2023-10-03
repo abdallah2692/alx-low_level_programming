94ea796676fbc667992a046219e0f4963a1c8e21
abdallah2692 <abdallahmotawea0pl@gmail.com>
2023-10-04 01:49:45 +0300

    recursive

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -28,9 +28,11 @@ int ret = 0;
 
 if (*s1 == 0)
 return (0);
-if (*s1 == *s2)
-ret += wildcmp(s1 + 1, s2 + 1);
 
+if (*s1 == *s2)
+{
+ret += wildcmp(s1 + 1, s2 + 1);
+}
 ret += inception(s1 + 1, s2);
 return (ret);
 }
@@ -47,15 +49,17 @@ int wildcmp(char *s1, char *s2)
 {
 int ret = 0;
 
-if (!*s1 && *s2 == '*' && !*move_past_star(*s2))
+if (!*s1 && *s2 == '*' && !*move_past_star(s2))
 return (1);
 if (*s1 = *s2)
 {
 if (!*s1)
+{
 return (1);
-
+}
 return (wildcmp(s1 +1, *s2 == '*' ? s2 : s2 + 1));
 }
+
 if (!*s1 || !s2)
 return (0);
 
@@ -65,8 +69,9 @@ s2 = move_past_star(s2);
 if (!*s2)
 return (1);
 if (*s1 = *s2)
+{
 ret += wildcmp(s1 + 1, s2 + 1);
-
+}
 ret += inception(s1, s2);
 return(!!ret);
 }
