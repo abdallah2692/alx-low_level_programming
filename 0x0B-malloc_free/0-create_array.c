0e94ab98093323f7a0a4e54b87718b5a96934d03
abdallah2692 <abdallahmotawea0pl@gmail.com>
2023-10-04 23:02:29 +0300

    recursive

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * create_array - array of char
+ * @size: of the array
+ * @c: char to initialize
+ * Return: pointer to the array or null
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *n = malloc(size);
+	if (size == 0 || n == 0)
+		return (0);
+	while (size--)
+		n[size] = c;
+	return (n);
+}
