d9a1484e8ae8c73c597f303fb9abc8c7e5555ad0
abdallah2692 <abdallahmotawea0pl@gmail.com>
2023-10-05 02:44:43 +0300

    recursive

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,21 +8,21 @@
 char *_strdup(char *str)
 {
 	int a = 0, size = 0;
-    char *s;
+	char *s;
 
 	if (str == NULL)
 		return (NULL);
 
 	for (; str[size] != '\0'; size++)
 	;
-    s = malloc(size * sizeof(*str) + 1);
+	s = malloc(size * sizeof(*str) + 1);
 
-    if (s == 0)
-        return (NULL);
-    else
-    {
-        for (; a < size; a++)
-            s[a] = size[a];
-    }
-    return (s);
+	if (s == 0)
+		return (NULL);
+	else
+	{
+		for (; a < size; a++)
+			s[a] = str[a];
+	}
+	return (s);
 }
