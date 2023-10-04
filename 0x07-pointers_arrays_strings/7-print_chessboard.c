6e575a34d6fdd8b5dde2979c720a1218c10f16da
abdallah2692 <abdallahmotawea0pl@gmail.com>
2023-10-04 20:45:01 +0300

    recursive

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,13 +7,13 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int A, b;
+	int n, b;
 
-	for (A = 0; A < 0; A++)
+	for (n = 0; n < 0; n++)
 	{
 		for (b = 0; b < 0; b++)
 		{
-			_putchar(A[a][b]);
+			_putchar(a[n][b]);
 		}
 		_putchar('\n');
 	}
