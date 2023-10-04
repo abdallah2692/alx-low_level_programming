4614df843ce40c2e0e87b33bd6d34ad65b9d707c
abdallah2692 <abdallahmotawea0pl@gmail.com>
2023-10-05 02:43:03 +0300

    recursive

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strdup - return a duplicated string
+ * @str: the duplicated string
+ * Return: 0
+*/
+
+char *_strdup(char *str)
+{
+	int a = 0, size = 0;
+    char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (; str[size] != '\0'; size++)
+	;
+    s = malloc(size * sizeof(*str) + 1);
+
+    if (s == 0)
+        return (NULL);
+    else
+    {
+        for (; a < size; a++)
+            s[a] = size[a];
+    }
+    return (s);
+}
