a89fc75ec0487e1fdff38d54fbbf6051b726f263
abdallah2692 <abdallahmotawea0pl@gmail.com>
2023-10-09 03:01:34 +0300

    recursive

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * *memset - fills memory
+ * @s: pointer
+ * @b: const
+ * @n:bytes
+ * Return: pointer s
+*/
+
+void *_memset(char *s, char a, unsigned int n)
+{
+	char *p = s;
+
+	while (n--)
+		*s++ = a;
+	return (p);
+}
+
+/**
+ * *_calloc - allocate memory
+ * @nmemd: length of array
+ * @size: size of element
+ * Return: a pointer
+*/
+
+
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *d;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+
+	if (d == 0)
+		return (NULL);
+
+	_memset(d, 0, sizeof(int) * nmemb);
+	return (d);
+}
