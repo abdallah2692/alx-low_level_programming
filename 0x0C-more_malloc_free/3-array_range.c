c6ca62f9734d779e329afa851ca6370b44bbdaa3
abdallah2692 <abdallahmotawea0pl@gmail.com>
2023-10-09 03:18:42 +0300

    recursive

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *array_range - concat two string
+ * @max: maximume
+ * @min: starting
+ * @n: num of bytes
+ * Return: array of pointer
+*/
+
+int *array_range(int min, int max)
+{
+	int a, b;
+	int *s;
+
+	if (min > max)
+		return (NULL);
+
+	a = max - min + 1;
+	s = malloc(sizeof(int) * a);
+	if (!s)
+		return(NULL);
+	for (b = 0; b < a; b++)
+		s[a] = min++;
+	return (s);
+}
