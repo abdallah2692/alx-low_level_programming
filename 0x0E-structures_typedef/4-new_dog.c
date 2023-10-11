e193132a193cba7ff96e380b26084936b71cd288
abdallah2692 <abdallahmotawea0pl@gmail.com>
2023-10-11 23:24:10 +0300

    struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -64,4 +64,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(d);
 		return (NULL);
 	}
+
+	(*d).owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if ((*d).owner == NULL)
+	{
+		free((*d).name)
+		free(d);
+		return (NULL);
+	}
 }
