cc9c02712dac0bcaad53e157bcf3cfcbe35b6928
abdallah2692 <abdallahmotawea0pl@gmail.com>
2023-10-13 01:12:47 +0300

    struct

--- 0x10-variadic_functions/3-main.c
+++ 0x10-variadic_functions/3-main.c
@@ -1,87 +1,12 @@
 #include "variadic_functions.h"
 
 /**
- * format_char - format
- * @separator: string separator
- * @ap: argu pointer
-*/
-void format_char(char *separator, va_list ap)
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	psintf("%s%c", separator, va_arg(ap, int));
-}
-
-
-/**
- * format_int - format
- * @separator: string separator
- * @ap: argu pointer
-*/
-void format_int(char *separator, va_list ap)
-{
-	psintf("%s%d", separator, va_arg(ap, int));
-}
-
-
-/**
- * format_float - format
- * @separator: string separator
- * @ap: argu pointer
-*/
-void format_float(char *separator, va_list ap)
-{
-	psintf("%s%f", separator, va_arg(ap, double));
-}
-
-/**
- * format_string - format
- * @separator: string separator
- * @ap: argu pointer
-*/
-void format_srtring(char *separator, va_list ap)
-{
-	char *str = va_arg(ap, char *);
-
-	switch ((int)(!str))
-		case 1;
-			str = "(nil)";
-	psintf("%s%s", separator, str);
-}
-
-
-
-
-/**
- * print_all - print the inputs num
- * @format: format
-*/
-
-
-void print_all(const char * const format, ...)
-{
-	int a = 0, b;
-	char *separator = "";
-	token_a tokens[] = {
-		{"c", format_char},
-		{"i", format_int},
-		{"f", format_float},
-		{"s", format_string},
-		{NULL, NULL}
-	};
-	va_start(ap, format);
-	while (format && format[a])
-	{
-		b = 0;
-		while (tokens[b].token)
-		{
-			if (format[a] == tokens[b].token[0])
-			{
-				tokens[b].f(separator, ap);
-				separator = ", ";
-			}
-			b++;
-		}
-		a++
-	}
-	printf("\n");
-	va_end(ap);
+    print_all("ceis", 'B', 3, "stSchool");
+    return (0);
 }
