6d0e0ee4638822ed69deca58ffe74342345c0b9c
abdallah2692 <abdallahmotawea0pl@gmail.com>
2023-11-01 06:08:31 +0200

    file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char fp[BUFSIZ * 8];
+	ssize_t a;
+	int i;
+
+	if (!filename || !letters)
+		return (0);
+	i = open(filename, O_RDONLY);
+	if (i == -1)
+		return (0);
+	a = read(i, &fp[0], letters);
+	a = write(STDOUT_FILENO, &fp[0], a);
+	close(i);
+	return (a);
+}
+
