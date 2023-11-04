9005582829094f92fb2aee2976f0e028f0b624bb
abdallah2692 <abdallahmotawea0pl@gmail.com>
2023-11-04 21:17:34 +0200

    filee 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,19 +9,28 @@
 
 int create_file(const char *filename, char *text_content)
 {
+	int fd;
+	ssize_t add = 0, len = _strlen(text_content);
+
 	if (!filename)
 		return (-1);
-
-	FILE *fp = fopen(filename, 600);
-	if (!fp)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
-	if (text_content != NULL)
-		fwrite(text_content, sizeof(text_content[0]), strlen(text_content), fp);
+	if (len)
+		add = write(fd, text_content, len);
+	close(fd);
+	return (add == len ? 1 : -1);
+}
 
-	if (fclose(fp) == EOF)
-		return (-1);
-	if (chmod(filename, S_IRUSR | S_IWUSR) == -1)
-	return (-1);
 
-	return (1);
+
+int _strlen(char *c)
+{
+	int a = 0;
+	if (!c)
+		return (0);
+	while (*c++)
+		a++;
+	return (a);
 }
